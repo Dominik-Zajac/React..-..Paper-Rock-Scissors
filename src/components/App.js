45e70b4bbb0d7d98d8fe386fac9dbfe0858fc6d5
Wyrafinowany <zajac_dominik@onet.pl>
2019-05-29 11:24:39 +0200

    Add new components

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import SelectMove from './SelectMove';
+import StartGame from './StartGame';
+import PanelLeft from './PanelLeft';
+import PanelRight from './PanelRight';
+import '../sass/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1 className='title-game'>Papier, kamień, nożyczki</h1>
+        <SelectMove />
+        <StartGame />
+        <PanelLeft />
+        <PanelRight />
+      </div>
+    );
+  }
+}
+
+export default App;
